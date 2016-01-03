918044e212052648691a46cefd7cbdd2493b8d94
Alex Zwir <alexzwir@gmail.com>
2016-01-03 13:47:15 -0200

    string 1 exercise

--- basic/string1.py
+++ basic/string1.py
@@ -68,8 +68,32 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-    # +++your code here+++
-    return
+    l=list(s)
+    print(l)
+
+    l_sem_primeira_posicao = l[1:]
+    print(l_sem_primeira_posicao)
+
+    first_l = l[0]
+    print(first_l)
+
+    for c in l_sem_primeira_posicao:
+        if(c != first_l):
+            print(c, first_l,'não é igual')
+            return c
+        else:
+            print(c,first_l,'é igual')
+            c = '*'
+            print(c,first_l,'é igual')
+            return c
+
+    return l_sem_primeira_posicao
+
+
+
+
+
+
 
 
 # D. MixUp
